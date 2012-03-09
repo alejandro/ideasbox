f47a1d90df37287c558de76805eef26837247a4d
Alejandro Morales <vamg008@gmail.com>
2012-03-08 21:26:00 -0600

    [kronos] Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# ideasbox
+
+ This is a **README**
+
+## Contributors
+
+- [Kronos](http://github.com/alejandromg/kronos 'npm install kronos') 
---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+/*
+ * ideasbox
+ * @date:Thu Mar 08 2012 21:25:59 GMT-0600 (CST)
+ * @name: index.js
+ * @licence: MIT
+*/
+
+
+module.exports = require('./lib/ideasbox');
+module.exports.run(8000);
