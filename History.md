89e1db7e080c63192607a84a046e3520cfc352e5
Alejandro Morales <vamg008@gmail.com>
2012-03-10 00:37:56 -0600

    [master] First working model

---
+++ History.md
@@ -0,0 +1,18 @@
+
+n.n.n / 2012-03-10 
+==================
+
+  * [master] Added index and more
+  * [master] Added foundation and more godness
+  * [master] jade support for tako :)
+  * [master] Added dependencies
+  * [kronos] Initial Commit
+
+n.n.n / 2012-03-09 
+==================
+
+  * [master] Added index and more
+  * [master] Added foundation and more godness
+  * [master] jade support for tako :)
+  * [master] Added dependencies
+  * [kronos] Initial Commit
---
+++ public/javascripts/ideas.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+  var loader = $('#loader')
+    , form = $('#idea')
+    , comment = $('#comment')
+    , submit = $('#send')
+    , successIdea = '<p> You can see your idea at:<a href="<%= url%>",title="Go to"><%= url%></a></p>'
+    , msg = $('#msg')
+    ;
+
+    submit.on('click', function(e){
+      e.preventDefault()
+      loader.removeClass('off')
+      msg.textContent = 'Posting...'
+      form.addClass('off')
+      var data = serialize()
+      $.post('/'+data.room + '/new',data,function(resp,status){
+        if (resp && status == 'success'){
+          var self = resp;
+          switch (resp.statusCode){
+            case 200:
+            case '200':
+              var tpl = _.template(successIdea)
+              comment.html(tpl({url:resp.url}));
+              break;
+            case '422':
+            case 422:
+              comment.html('<h4> Error</h4><span> Idea name already Exists</span>')
+              break;
+            default:
+              comment.html('<h4> 500 Internal Server Error</h4><span> Try again in a little</span>')
+              break;
+          }
+        }
+      });
+    });
+    function getValues (name){
+      return $('[name="'+name+'"]')
+    }
+    window.serialize = function(){
+      return {
+          name: getValues('name').val()
+        , description : getValues('description').val()
+        , author:{
+              name: getValues('user[name]').val()
+            , from : getValues('user[from]').val()
+            , email: getValues('user[email]').val()
+          }
+        , room : getValues('user[from]').val()
+      }
+    }
+
+})
